9f92fc8b714ddd4b2e818ab20c02e682c9ff2a65
Nadia Lee <miss.nadia.lee@gmail.com>
2017-10-15 19:39:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,13 +4,13 @@ def turn_count(board)
     (turn == "X") || (turn == "O") ? counter += 1 : counter += 0
   end
     puts "#{counter}"
+    num = counter
 end
 
 def current_player(board)
-  def turn_count(board)
-  end
-  turn_count(board) % 2 == 0 ? "X" : "O"
+  num % 2 == 0 ? "X" : "O"
 end
 
 board = ["O"," "," "," ","X"," "," "," ","X"]
 turn_count(board)
+
