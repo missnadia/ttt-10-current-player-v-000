9e23068e2f89cb02d3b0da95fce6ef27f15a910d
Nadia Lee <miss.nadia.lee@gmail.com>
2017-10-15 18:20:47 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 
-def turn_count(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+def turn_count(board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"])
   counter = 0
   board.each do |turn|
     puts counter
