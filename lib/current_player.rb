2d2a7883202322292d6fe5bfcd578b4e12b61bfe
Nadia Lee <miss.nadia.lee@gmail.com>
2017-10-15 19:10:05 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,7 +7,7 @@ def turn_count(board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"])
 end
 
 def current_player(board)
-  (turn_count(board) % 2) == 0 ? puts "X" : puts "O"
+  (turn_count(board) % 2) == 0 ? "X" : "O"
 end
 
 turn_count(board)
