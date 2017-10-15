5302657ee29805450487d63289c5b5ad1198333a
Nadia Lee <miss.nadia.lee@gmail.com>
2017-10-15 19:09:18 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,9 @@ def turn_count(board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"])
   counter = 0
   board.each do |turn|
     (turn == "X") || (turn == "O") ? counter += 1 : counter += 0
-    puts counter
   end
+  puts counter
+end
 end
 
 def current_player(board)
